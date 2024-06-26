a332d5e01147a1c0105f3d6eba23ac5a34de913a
maddie <dmadeline2003@gmail.com>
2024-06-26 10:21:50 +0800

    added textfield

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Login from "./pages/Login";
--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Login.css'; 
 
 export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Login button clicked");
+  };
+
   return (
-    <div>
-      Hello World hahahah
+    <div className="loginContainer">
+      <input type="text" id="username" className="loginInput" placeholder="Username" />
+      <input type="password" id="password" className="loginInput" placeholder="Password" />
+      <button className="loginButton" onClick={handleSubmit}>
+        Login
+      </button>
     </div>
-  )
+  );
 }
