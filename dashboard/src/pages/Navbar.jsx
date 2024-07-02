3eb172563d2bce10dacd8255ef69ead47f5d30b5
maddie <dmadeline2003@gmail.com>
2024-07-02 10:20:57 +0800

    fixed navbar

--- dashboard/src/pages/Navbar.jsx
+++ dashboard/src/pages/Navbar.jsx
@@ -4,35 +4,62 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
 
 export default function Navbar() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const isAdminOrOwner =
+        user && (user.role === "Admin" || user.role === "Owner");
+    const isLoggedIn = true;
+
+    console.log("User:", user);
+    console.log("Is Admin or Owner:", isAdminOrOwner);
 
     const navigate = useNavigate();
     const handleHome = () => {
         navigate("/home");
     };
 
-  return (
-    <div className="navbar">
-        <div className="navbar-items">
-        
-        <img src={logo} alt="Logo" style={{ marginRight: "auto" }} onClick={handleHome}/> 
-        
-        
-            <div
-                className="navbar-items-section"
-                style={{
-                    display: "flex",
-                    gap: "50px",
-                    justifyContent: "flex-end",
-                    marginRight: "40px",
-                }}
-            >
-                <Link to="/home">HOME</Link>
-                <Link to="/menu">MENU</Link>
-                <Link to="/manageuser">MANAGE USER</Link>
-                <Link to="/manageproduct">MANAGE PRODUCT</Link>
-                <Link to="/login">Login</Link>
+    const handleLogout = () => {
+        console.log("Logging out...");
+        navigate("/login");
+    };
+
+    return (
+        <div className="navbar">
+            <div className="navbar-items">
+                <img
+                    src={logo}
+                    alt="Logo"
+                    style={{ marginRight: "auto" }}
+                    onClick={handleHome}
+                />
+
+                <div
+                    className="navbar-items-section"
+                    style={{
+                        display: "flex",
+                        gap: "50px",
+                        justifyContent: "flex-end",
+                        marginRight: "40px",
+                    }}
+                >
+                    <Link to="/home">HOME</Link>
+                    <Link to="/menu">MENU</Link>
+
+                    {isAdminOrOwner ? (
+                        <>
+                            <Link to="/manageuser">MANAGE USER</Link>
+                            <Link to="/manageproduct">MANAGE PRODUCT</Link>
+                        </>
+                    ) : null}
+
+                    {isLoggedIn ? (
+                        <a href="#" onClick={handleLogout}>
+                            LOGOUT
+                        </a>
+                    ) : (
+                        <Link to="/login">Login</Link>
+                    )}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 }
