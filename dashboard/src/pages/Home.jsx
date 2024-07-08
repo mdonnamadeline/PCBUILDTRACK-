b4d891723ee9a3b4fd2935feacb9091f27a7031e
maddie <dmadeline2003@gmail.com>
2024-07-08 08:31:55 +0800

    image and css for the landing page

--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar';
+import Zinger from '../images/zinger.jpg'
+import Dasurv from '../images/dasurv.jpg'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
        <Navbar /> 
-       <div>wala pa po</div>
+       <div className='homecon'>
+       <img src={Zinger} alt="Zinger" />
+       <img src={Dasurv} alt="Dasurv" />
+       </div>
     </div>
   )
 }
