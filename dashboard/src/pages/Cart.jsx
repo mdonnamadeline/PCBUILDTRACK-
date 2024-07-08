39e92b51b7e08908336554b91f7ac29aa8aef16b
maddie <dmadeline2003@gmail.com>
2024-07-08 10:00:54 +0800

    added message only no funtion yet

---
+++ dashboard/src/pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <div>
+      dito makikita mga inadd mo sa cart!!!!!
+    </div>
+  )
+}
---
+++ dashboard/src/pages/Payment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Payment() {
+  return (
+    <div>
+      pipili ka dito kung union bank and metro bank!!!!
+    </div>
+  )
+}
