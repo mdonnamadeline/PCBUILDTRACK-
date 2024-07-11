065034e32be7ec566cdca088bede27f807025aed
maddie <dmadeline2003@gmail.com>
2024-07-11 09:19:11 +0800

    added disabled to the schema

--- server/models/menu.model.js
+++ server/models/menu.model.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const menuSchema = new Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true }
-}, { collection: 'menu-data' });
+const menuSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+        disabled:{ type: Boolean, required: true }
+    },
+    { collection: "menu-data" }
+);
 
-const Menu = mongoose.model('Menu', menuSchema);
+const Menu = mongoose.model("Menu", menuSchema);
 
 module.exports = Menu;
