19d4ffc15b79fbe8def356e04ed93762ab09c6fa
maddie <dmadeline2003@gmail.com>
2024-07-12 07:55:01 +0800

    remove navbar

--- dashboard/src/pages/Dashboard.jsx
+++ dashboard/src/pages/Dashboard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Sidebar from './Sidebar' ;
-// import Navbar from "./Navbar";
 import './Dashboard.css';
 
 export default function Dashboard() {
     return (
         <div className="dashboard">
             <Sidebar />
-            {/* <Navbar /> */}
+
             <div className="con">
                 <h1>ADMIN DASHBOARD</h1>
             </div>
