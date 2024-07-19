a0c4fb2dfd265bb4d91aba92fc9642af66300404
maddie <dmadeline2003@gmail.com>
2024-07-19 09:07:49 +0800

    fix navigate

--- dashboard/src/pages/Sidebar.jsx
+++ dashboard/src/pages/Sidebar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import "./Sidebar.css";
 import logo from "../images/logo.png";
 
 export default function Sidebar() {
     const handleHome = () => {
-        navigate("/home");
+        Navigate("/home");
     };
 
     return (
