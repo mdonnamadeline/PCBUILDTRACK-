1de5054f9ed562326137d923298a6a5d50fae8fa
Seanti <seanpizzaspark@gmail.com>
2024-07-22 21:23:24 +0800

    integrated api for business

--- dashboard/src/pages/Business.jsx
+++ dashboard/src/pages/Business.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Card, Grid } from "@mui/material";
 import Sidebar from "./Sidebar";
-import Cafe from "../logo/CafeReyes.png";
-import Ohmart from "../logo/Ohmart.png";
-import savemore from "../logo/savemore.png";
-import '../styles/Business.css';
-
-const data = [
-  {
-    id: 1,
-    link: "http://192.168.10.18:3000",
-    imageUrl: Cafe,
-  },
-  {
-    id: 2,
-    link: "http://192.168.10.21",
-    imageUrl: Ohmart,
-  },
-  {
-    id: 3,
-    link: "http://192.168.10.25",
-    imageUrl: savemore,
-  },
-];
+import "../styles/Business.css";
 
 export default function Business() {
-  return (
-    <div className="flex-container">
-      <Sidebar className="sidebar" />
-      <div className="main-content">
-        <div className="head-con">
-          YOU BILI DITO
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            try {
+                const projectsResponse = await axios.get(
+                    "http://192.168.10.18:1337/api/projects"
+                );
+                setProjects(projectsResponse.data);
+            } catch (error) {
+                console.error("Error fetching projects:", error);
+            }
+        };
+
+        fetchProjects();
+    }, []);
+
+    return (
+        <div className="flex-container">
+            <Sidebar className="sidebar" />
+            <div className="main-content">
+                <div className="head-con">PARTNERS</div>
+                <Grid container spacing={2} className="cards-container">
+                    {projects.map((item) => {
+                        const imageUrl = `http://192.168.10.18:1337/uploads/${item.image}`;
+
+                        return (
+                            <Grid
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                lg={2.4}
+                                key={item._id}
+                            >
+                                <Card className="business-card">
+                                    <a
+                                        href={item.link}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img src={imageUrl} alt={item.name} />
+                                    </a>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </div>
         </div>
-        <Grid container spacing={2} className="cards-container">
-          {data.map((item) => (
-            <Grid item xs={12} sm={6} md={4} lg={2.4} key={item.id}>
-              <Card className="business-card">
-                <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  <img src={item.imageUrl} alt={`Item ${item.id}`} />
-                </a>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    </div>
-  );
+    );
 }
