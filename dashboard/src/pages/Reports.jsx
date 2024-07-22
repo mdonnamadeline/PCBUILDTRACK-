c38be06b97e8949af12799b747b665a63d58fca5
maddie <dmadeline2003@gmail.com>
2024-07-23 07:46:33 +0800

    clean code

--- dashboard/src/pages/Reports.jsx
+++ dashboard/src/pages/Reports.jsx
@@ -29,7 +29,7 @@ const StyledTableCell = styled(TableCell)({
 const formatDate = (dateString) => {
   const date = new Date(dateString);
   const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const month = String(date.getMonth() + 1).padStart(2, '0'); 
   const year = date.getFullYear();
   
   let hours = date.getHours();
@@ -37,7 +37,7 @@ const formatDate = (dateString) => {
   const seconds = String(date.getSeconds()).padStart(2, '0');
   const ampm = hours >= 12 ? 'PM' : 'AM';
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours ? hours : 12; 
   hours = String(hours).padStart(2, '0');
 
   return `${day}/${month}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
