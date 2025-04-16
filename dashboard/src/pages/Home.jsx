04bed08c7974f6f12707b95f802b2246f7a40c76
Donna <dmadeline2003@gmail.com>
2025-04-16 15:44:01 +0800

    fix color sidebar and fix home page also added footer to the home page

--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Card, Grid } from "@mui/material";
 import Navbar from "./Navbar";
-import Zinger from "../images/zinger.jpg";
-import Dasurv from "../images/dasurv.jpg";
+import imageH1 from "../images/imageH1.jpg";
+import imageH2 from "../images/imageH2.jpg";
 import "../styles/Home.css";
-import "../styles/Business.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     const [projects, setProjects] = useState([]);
@@ -28,14 +27,32 @@ export default function Home() {
     return (
         <div className="home">
             <Navbar />
-            <div className="">
-                {/* //add contegories */}
-           
             <div className="homecon">
-                <img src={Zinger} alt="Zinger" />
-                <img src={Dasurv} alt="Dasurv" />
-            </div>    
+                <div className="imageContainer">
+                    <img src={imageH1} alt="Image 1" className="landscapeImage" />
+                    <img src={imageH2} alt="Image 2" className="landscapeImage" />
+                </div>
+                <div className="footer">
+                    <div className="footerSection">
+                        <h3>ADDRESS</h3>
+                        <p>Branches</p>
+                    </div>
+                    <div className="footerSection">
+                        <h3>SITEMAP</h3>
+                        <Link to="/home"><p>Home</p></Link>
+                        <Link to="/menu"><p>Browse Product</p></Link>
+                        <p>Contact Us</p>
+                    </div>
+                    <div className="footerSection">
+                        <h3>About PCBuild Track</h3>
+                        <Link to="/signup"><p>Register</p></Link>
+                        <Link to="/login"><p>Login</p></Link>
+                    </div>
+                    <div className="footerCopyright">
+                        <p>Copyright © 2010-2025 PCBuild Track. All rights reserved.</p>
+                    </div>
+                </div>
             </div>
-            </div>
+        </div>
     );
 }
